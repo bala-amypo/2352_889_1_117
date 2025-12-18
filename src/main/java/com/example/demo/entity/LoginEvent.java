6228e792716fc42a61c55762ca909049c3024e6e
bala-amypo <bala@amypo.in>
2025-12-18 08:49:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/LoginEvent.java
+++ src/main/java/com/example/demo/entity/LoginEvent.java
@@ -2,5 +2,13 @@
 package com.example.demo.entity;
 
 public class LoginEvent{
+    private Long id;
+    private Long userId;
+    private String ipAddress;
+    private String location;
+    private String deviceld;
+    private LocalDateTime timestamp;
+    
+
     
 }
