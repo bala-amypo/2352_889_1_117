8aa1c41e39091b0d5f03f077fdf95b1235eaeada
bala-amypo <bala@amypo.in>
2025-12-18 08:34:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/DeviceProfile.java
+++ src/main/java/com/example/demo/entity/DeviceProfile.java
@@ -3,5 +3,9 @@ package com.example.demo.entity;
 
 public class DeviceProfile{
     private Long id;
+    private String employeeld;
+    private String username;
+    private String email;
+    private String password
 
 }
--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -2,5 +2,13 @@
 package com.example.demo.entity;
 
 public class UserAccount{
+    @Id
+    private Long id;
+    private String employeeld;
+    private String username;
+    private String email;
+    private String password;
+    private String ADMIN;
     
+
 }
