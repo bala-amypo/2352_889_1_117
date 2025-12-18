8bb064b487f8bb83918fb5d994a4bf13257d5f8d
bala-amypo <bala@amypo.in>
2025-12-18 16:46:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,15 @@
+
+package com.example.demo.security;
+
+public class JwtUtil {
+
+    private final String secret;
+    private final long validityInMs;
+    private final boolean isTestMode;
+
+    public JwtUtil(String secret, long validityInMs, boolean isTestMode) {
+        this.secret = secret;
+        this.validityInMs = validityInMs;
+        this.isTestMode = isTestMode;
+    }
+}
