f7619151bb5160e828c899850cb2a94649096533
bala-amypo <bala@amypo.in>
2025-12-20 06:03:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,20 +2,19 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/auth")
-@Tag(name = "auth-controller")
+@Tag(name = "Auth Controller")
 public class AuthController {
 
     @PostMapping("/register")
-    public String register() {
+    public String register(@RequestBody Object request) {
         return "User registered";
     }
 
     @PostMapping("/login")
-    public String login() {
-        return "Login successful";
+    public String login(@RequestBody Object request) {
+        return "User logged in";
     }
 }
