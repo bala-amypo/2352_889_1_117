7342210daf7caff9c15f52a66180119bace1b9b4
bala-amypo <bala@amypo.in>
2025-12-20 05:31:49 +0000

    User commit

--- src/main/java/com/example/demo/service/LoginEventService.java
+++ src/main/java/com/example/demo/service/LoginEventService.java
@@ -2,30 +2,29 @@
 package com.example.demo.service;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
 import com.example.demo.entity.LoginEvent;
 import com.example.demo.repository.LoginEventRepository;
 import com.example.demo.util.RuleEvaluationUtil;
 
+@Service
 public class LoginEventService {
 
     private final LoginEventRepository repo;
     private final RuleEvaluationUtil util;
 
-    // EXACT constructor order (MANDATORY)
     public LoginEventService(LoginEventRepository repo,
                              RuleEvaluationUtil util) {
         this.repo = repo;
         this.util = util;
     }
 
-    // Controller calls this
     public LoginEvent record(LoginEvent event) {
         LoginEvent saved = repo.save(event);
         util.evaluateLoginEvent(saved);
         return saved;
     }
 
-    // Controller calls this
     public List<LoginEvent> suspicious(Long userId) {
         return repo.findByUserIdAndLoginStatus(userId, "FAILED");
     }
