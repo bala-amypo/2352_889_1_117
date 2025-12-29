f6559457aa7696ff81d7ee984d1738c85b77f8ff
bala-amypo <bala@amypo.in>
2025-12-29 03:04:43 +0000

    User commit

--- src/main/java/com/example/demo/util/RuleEvaluationUtil.java
+++ src/main/java/com/example/demo/util/RuleEvaluationUtil.java
@@ -46,31 +46,31 @@
 // }
 
 
-package com.example.demo.util;
+// package com.example.demo.util;
 
-import com.example.demo.entity.*;
-import com.example.demo.repository.*;
+// import com.example.demo.entity.*;
+// import com.example.demo.repository.*;
 
-public class RuleEvaluationUtil {
-    private final PolicyRuleRepository ruleRepo;
-    private final ViolationRecordRepository violationRepo;
+// public class RuleEvaluationUtil {
+//     private final PolicyRuleRepository ruleRepo;
+//     private final ViolationRecordRepository violationRepo;
 
-    public RuleEvaluationUtil(PolicyRuleRepository ruleRepo,
-                              ViolationRecordRepository violationRepo) {
-        this.ruleRepo = ruleRepo;
-        this.violationRepo = violationRepo;
-    }
+//     public RuleEvaluationUtil(PolicyRuleRepository ruleRepo,
+//                               ViolationRecordRepository violationRepo) {
+//         this.ruleRepo = ruleRepo;
+//         this.violationRepo = violationRepo;
+//     }
 
-    public void evaluateLoginEvent(LoginEvent e) {
-        for (PolicyRule r : ruleRepo.findByActiveTrue()) {
-            if (r.getConditionsJson() != null &&
-                e.getLoginStatus() != null &&
-                r.getConditionsJson().contains(e.getLoginStatus())) {
+//     public void evaluateLoginEvent(LoginEvent e) {
+//         for (PolicyRule r : ruleRepo.findByActiveTrue()) {
+//             if (r.getConditionsJson() != null &&
+//                 e.getLoginStatus() != null &&
+//                 r.getConditionsJson().contains(e.getLoginStatus())) {
 
-                ViolationRecord v = new ViolationRecord();
-                v.setSeverity(r.getSeverity());
-                violationRepo.save(v);
-            }
-        }
-    }
-}
+//                 ViolationRecord v = new ViolationRecord();
+//                 v.setSeverity(r.getSeverity());
+//                 violationRepo.save(v);
+//             }
+//         }
+//     }
+// }
